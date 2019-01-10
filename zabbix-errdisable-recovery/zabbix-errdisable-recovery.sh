2a317dabe618e06dc26a6716c6e3fd3371753344
nryzhkov <nryzhkov@vbrr.ru>
2019-01-10 12:15:59 +0000

    initial

--- zabbix-errdisable-recovery/zabbix-errdisable-recovery.sh
+++ zabbix-errdisable-recovery/zabbix-errdisable-recovery.sh
@@ -1,1 +1,4 @@
-sh
+#!/bin/bash
+
+cd /etc/zabbix/alertscripts/zabbix-errdisable-recovery
+/usr/local/bin/ansible-playbook clear-portsecurity.yml --vault-password-file=vault/vault_password -e interface=$1 -i $2,
