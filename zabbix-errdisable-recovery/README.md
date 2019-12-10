0fba6ae8c2e9dcb7f2d7a40494b7464588d2f7d0
IDQDD <nryzhkov@gmail.com>
2019-12-10 11:10:55 +0300

    clearportsecurity now closes zabbix events via zabbix_api; modified README as well

--- zabbix-errdisable-recovery/README.md
+++ zabbix-errdisable-recovery/README.md
@@ -1,2 +1,26 @@
-скрипт разблокировки заблокированных механизмом **PortSecurity** портов на коммутаторах **Cisco** средствами _Zabbix_  
-подробней в статье по [ссылке](http://dokuwiki.msk.vbrr.loc/doku.php?id=monitoring:zabbix_traps_portsecurity#%D0%BE%D1%87%D0%B8%D1%81%D1%82%D0%BA%D0%B0_%D0%B7%D0%B0%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE_%D0%BF%D0%BE%D1%80%D1%82%D0%B0_%D1%81%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B2%D0%B0%D0%BC%D0%B8_zabbix)
+Скрипт разблокировки заблокированных механизмом **PortSecurity** портов на коммутаторах **Cisco** средствами _Zabbix_  
+Для чего он нужен написано в статье по [ссылке](http://dokuwiki.msk.vbrr.loc/doku.php?id=monitoring:zabbix_traps_portsecurity#%D0%BE%D1%87%D0%B8%D1%81%D1%82%D0%BA%D0%B0_%D0%B7%D0%B0%D0%B1%D0%BB%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE_%D0%BF%D0%BE%D1%80%D1%82%D0%B0_%D1%81%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B2%D0%B0%D0%BC%D0%B8_zabbix)
+
+## Установка скрипта
+
+1. склонировать файлы из репозитория *git pull/clone ...*
+* доставить необходимые пакеты *pip install -r requirements.txt*
+* создать файл *defaults/creds.yml* с таким содержимым
+
+    ```yml
+    user: cisco
+    pass: cisco
+    zabbix_url: https://zabbix.acmeloc
+    zabbix_user: api
+    zabbix_passwd: api
+    ```
+* создать файл *vault/vault_password* с паролем
+* зашифровать *defaults/creds.yml* паролем из *vault/vault_password*:     
+    > user@host:~$ ansible-vault encrypt --vault-password-file vault/vault_password defaults/creds.yml
+* скопировать все файлы в директорию */etc/zabbix/alertscripts/zabbix-errdisable-recovery/*  
+    и затем модифицировать права на файлы выполнив команду:
+    > user@host:~$ chown -R zabbix /etc/zabbix/alertscripts/zabbix-errdisable-recovery/
+* в веб-морде *Zabbix* создать **Action** и настроить действия в вкладке **Acknowledgement operations** следующим ообразом:
+![](../pics/errdisable-recovery-zabbix-ack.png)
+    > Строка запуска:   
+        /usr/bin/python3 /etc/zabbix/alertscripts/zabbix-errdisable-recovery/clear-portsecurity.py {ITEM.LASTVALUE} {HOST.HOST} {EVENT.ID} {EVENT.STATUS}
--- zabbix-errdisable-recovery/clear-portsecurity.py
+++ zabbix-errdisable-recovery/clear-portsecurity.py
@@ -14,13 +14,17 @@ commands = []
 
 # sys.argv
 
-if len(sys.argv) == 3 :
+if len(sys.argv) == 5 :
     ifname = sys.argv[1]
     hostname = sys.argv[2]    
-elif len(sys.argv) == 4 :
+    eventid = sys.argv[3]
+    eventstatus = sys.argv[4]
+elif len(sys.argv) == 6 :
     ifname = sys.argv[1]
     mac_str = sys.argv[2]
     hostname = sys.argv[3]
+    eventid = sys.argv[4]
+    eventstatus = sys.argv[5]
 
     mac_str = netaddr.EUI(sys.argv[2])
     mac_str.dialect = netaddr.mac_cisco
@@ -28,6 +32,12 @@ elif len(sys.argv) == 4 :
 else:
     msg = "Missing or invalid arguments"
     raise ValueError(msg)
+
+# if event.status is RESOLVED it means the script has already been executed 
+# but it was started by the zabbix server again because the previous start made it acknowledged
+# we must exit now to prevent event ack loop
+if eventstatus != 'PROBLEM':
+    sys.exit()
 
 command = "clear port-security sticky interface {}".format(ifname)
 commands.append(command)
@@ -52,16 +62,43 @@ with open(vault_password_file, "r") as fp:
     vault = Vault(password)
     vaultdata = vault.load(open(vault_file).read())
 
-# data = { 'user': val1, 'pass': val2}    
-
-# napalm
+# vaultdata = { 'user': val1, 'pass': val2, 'zabbix_url': val3, 'zabbix_user': val4, 'zabbix_passwd': val5}    
+# execute cli commands on a cisco switch using NAPALM
 import napalm
+from datetime import datetime
 driver = napalm.get_network_driver("ios")
 
 with driver(hostname, vaultdata['user'], vaultdata['pass']) as device:
     device.open()
     res = device.cli(commands)
     with open(log_file, "a+") as fl:
-#        print(res)
         logstr = str(datetime.now()) + ' - the following commands were executed on a {}:\n\t - '.format(hostname) + '\n\t - '.join(commands) + '\n'
         fl.write(logstr)
+        
+        # we have just cleared a portsecurity event from a switch but there is a traphandler on the server algol that could still be processing the last trap from a switch
+        # so wait for 10 seconds before closing event to prevent any race conditions
+        from time import sleep
+        print("Waiting for 10 sec...\n")
+        sleep(10)
+
+        # Closing the zabbix event using zabbixAPI 
+        from pyzabbix.api import ZabbixAPI, ZabbixAPIException
+        zapi = ZabbixAPI(url=vaultdata['zabbix_url'], user=vaultdata['zabbix_user'], password=vaultdata['zabbix_passwd'])
+        if zapi:
+            fl.write("Closing Zabbix event {}\n".format(eventid))
+            try:
+                result = zapi.do_request('event.acknowledge', 
+                    {
+                        "eventids": eventid,
+                        "message": "PortSecurity Problem Resolved.",
+                        "action": 1
+                    })
+                if result['id'] == '1':
+                    print("Event closing: Success")
+                    fl.write("Zabbix event {} has been closed successfully\n".format(eventid))
+                else:
+                    fl.write("Zabbix event {} closing Failed\n".format(eventid))
+                    
+            except ZabbixAPIException as err:
+                fl.write(str(err))
+                print("Event closing: Fail")
